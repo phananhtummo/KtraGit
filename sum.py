5103e77823de88b7a1680aebba3f6a6d74f29fbf
PhanAnhTu-3616 <phananhtu12c4thd@gmail.com>
2026-04-02 17:57:59 +0700

    tong 3 so

---
+++ sum.py
@@ -0,0 +1,9 @@
+print("=== Tính tổng 3 số ===")
+
+a = float(input("Nhập số 1: "))
+b = float(input("Nhập số 2: "))
+c = float(input("Nhập số 3: "))
+
+tong = a + b + c
+
+print(f"\nKết quả: {a} + {b} + {c} = {tong}")
